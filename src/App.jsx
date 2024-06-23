a687ea164b035e38b9ffb9814d894214b3fe974d
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-06-23 19:00:10 +0200

    fix import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import FrameLayout from "./Layouts/FrameLayout/FrameLayout";
 import InteractiveSphere from "./components/InteractiveSphere/InteractiveSphere";
-import TorusKnot from "./components/TorusKnot/TorusKnot";
 
 function App() {
   return (
