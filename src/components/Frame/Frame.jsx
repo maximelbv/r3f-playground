43c62106526c1f8489e1d38e8502413c15fa4843
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-06-23 18:15:23 +0200

    create animation with sphere & torus

--- src/components/Frame/Frame.jsx
+++ src/components/Frame/Frame.jsx
@@ -1,16 +1,47 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
-const Cube = ({ position }) => {
+const Cube = ({ position, color }) => {
   const ref = useRef();
   useFrame((state, delta) => {
     ref.current.rotation.x += delta;
-    ref.current.rotation.y += delta;
+    ref.current.rotation.y += delta * 2;
+    ref.current.position.y = Math.sin(state.clock.elapsedTime) * 2;
   });
   return (
     <mesh position={position} ref={ref}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"orange"} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const Sphere = ({ position, color, args }) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.position.x = Math.sin(state.clock.elapsedTime) * 3;
+  });
+
+  return (
+    <mesh position={position} ref={ref}>
+      <sphereGeometry args={args} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const Torus = ({ rotation, position, color, args }) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta;
+  });
+
+  return (
+    <mesh rotation={rotation} position={position} ref={ref}>
+      <torusGeometry args={args} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
@@ -19,9 +50,17 @@ const Frame = () => {
   return (
     <div style={{ width: "100svw", height: "100svh" }}>
       <Canvas>
-        <ambientLight intensity={1} />
+        <ambientLight intensity={2} />
         <directionalLight position={[0, 0, 5]} />
-        <Cube />
+        <group rotation={[0, 0, 0]}>
+          <Sphere position={[3, 0, 0]} color={"#1874F2"} args={[1, 64, 32]} />
+          <Torus
+            rotation={[0, 2, 0]}
+            position={[0, 0, 0]}
+            color={"#f2a918"}
+            args={[1.2, 0.2, 30, 30]}
+          />
+        </group>
       </Canvas>
     </div>
   );
